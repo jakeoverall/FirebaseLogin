47dcd8f88cb5e9b410e842cfcded316a11a17a83
Jake Overall <joverall22@gmail.com>
2014-10-08 09:59:01 -0600

    firebase Login

--- authService.js
+++ authService.js
@@ -1,19 +1,22 @@
 var app = angular.module('firebaseLogin');
 
 app.service('authService', function(){
-
+	//Just a reference to the firebase endpoint
 	var firebaseUrl = 'https://devlogin.firebaseio.com/'
+	//Creates an object using the Firebase Constructor with our endpoint passed in 
 	var firebaseLogin = new Firebase(firebaseUrl);
+
+	//login method to be called from our controller. The callback is then passed the authenticated user
 
 	this.login = function(user, cb){
 		firebaseLogin.authWithPassword({
-			email    : user.email,
+			email    : user.email,    //Email and Password come from our login form 
 			password : user.password
 		}, function(err, authData) {
 			if (err) {
 				switch (err.code) {
 					case "INVALID_EMAIL":
-			        // handle an invalid email
+			        // handle an invalid email 
 			        case "INVALID_PASSWORD":
 			        // handle an invalid password
 			        default:
@@ -21,7 +24,7 @@ app.service('authService', function(){
 			} else if (authData) {
 			    // user authenticated with Firebase
 			    console.log("Logged In! User ID: " + authData.uid);
-			    cb(authData);
+			    cb(authData); //gives the authenticated user to our callback
 			}
 		});
 	}
@@ -39,7 +42,7 @@ app.service('authService', function(){
 					}, function(err, authData) {
 				  if (authData) {
 				  	authData.name = user.name;
-				  	authData.id = user.name.split(' ').join('_');
+				  	authData.timestamp = new Date().toISOString();
 				    firebaseLogin.child('users').child(authData.uid.replace('simplelogin:', '')).set(authData);
 				    cb(authData);
 				  } else {
